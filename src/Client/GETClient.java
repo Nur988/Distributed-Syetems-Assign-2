7c5302ff73b6fe5f46692b5353a7f2ba6ff6d770
Nur988 <proteas988@gmail.com>
2024-09-25 19:32:31 +0930

    Lamport Clock class is created , will move to integrate it in the Servers

--- src/Client/GETClient.java
+++ src/Client/GETClient.java
@@ -1,26 +1,42 @@
 package Client;
 
+//import org.json.JSONObject;
+
 import java.io.*;
 import java.net.*;
 import java.util.*;
+//import org.json.JSONArray;
+//import org.json.JSONObject;
 
-//import  org.json.simple.JSONObject;
+
 
 public class GETClient {
     public static void main(String[] args) throws IOException {
 
-            Socket socket =new Socket("localhost",6666);
-            DataOutputStream outstream =new DataOutputStream(socket.getOutputStream());
+            try(Socket socket =new Socket("localhost",6666))
+            {
+                    BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                    BufferedWriter out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
 
-            while(true) {
-                    System.out.println("Client: ");
-                    Scanner scanner = new Scanner(System.in);
-                    String input = scanner.nextLine();
-                    outstream.writeUTF(input);
+                    out.write("GET\n");
+                    out.flush();
+
+                    //Receive response and print out the response
+                    String resp =  in.readLine();
+                    System.out.println("Aggregation Server response");
+                    System.out.println(resp);
+
+
+
+
+
+
             }
-//            outstream.writeUTF("Hello Server");
-//            outstream.flush();
-//            outstream.close();
-//            socket.close();
+            catch(IOException e)
+            {
+                    e.printStackTrace();
+            }
+
+
 
 }}
